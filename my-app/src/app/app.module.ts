d23532606c76cf54c6ba6849b4b5cc5e9d700eaa
Dyshine Hill <eyedolarts@gmail.com>
2022-06-19 13:31:16 -0500

    created component using terminal

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -5,8 +5,9 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 @NgModule({
-  declarations: [AppComponent, ServerComponent],
+  declarations: [AppComponent, ServerComponent, ServersComponent],
   imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
