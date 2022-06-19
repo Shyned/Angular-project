f20283916465f9d77dabe00824ec890199aaa784
Dyshine Hill <eyedolarts@gmail.com>
2022-06-19 00:50:23 -0500

    created component

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
 
+import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ServerComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
