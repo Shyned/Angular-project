d64db812fee89cd2f480235606c5edf0de3de5bd
Dyshine Hill <eyedolarts@gmail.com>
2022-06-24 00:20:55 -0500

    created two way binding input field

--- my-app/src/app/servers/servers.component.ts
+++ my-app/src/app/servers/servers.component.ts
@@ -1,13 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
-  template: ` <app-server></app-server>
-    <app-server></app-server>`,
+  selector: '[app-servers]',
+  // template: ` <app-server></app-server>
+  //   <app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
-  constructor() {}
+  allowNewServer = false;
+  severCreationStatus = 'No sever was created!';
+  serverName = 'Testserver';
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit(): void {}
+  onCreateSever() {
+    this.severCreationStatus = 'Server was created';
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
